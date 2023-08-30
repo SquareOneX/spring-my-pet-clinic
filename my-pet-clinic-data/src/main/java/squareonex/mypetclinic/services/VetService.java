ee1b6a04b52e1d5cb2b7c1eda577730185628434
lukas_laptop <lukasreiche@outlook.de>
2023-08-30 17:36:27 +0200

    Implement interfaces for services
    Closes #5

---
+++ my-pet-clinic-data/src/main/java/squareonex/mypetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package squareonex.mypetclinic.services;
+
+import squareonex.mypetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet owner);
+
+    Set<Vet> findAll();
+}
+
