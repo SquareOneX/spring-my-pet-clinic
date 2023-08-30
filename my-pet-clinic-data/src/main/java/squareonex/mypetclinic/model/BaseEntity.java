84dd10c08da86840ac03954cc949d8e669e01827
lukas_laptop <lukasreiche@outlook.de>
2023-08-30 18:11:20 +0200

    Added BaseEntity with id attribute
    Pet and Person inherit id from BaseEntity
    
    Closes #10

---
+++ my-pet-clinic-data/src/main/java/squareonex/mypetclinic/model/BaseEntity.java
@@ -0,0 +1,25 @@
+package squareonex.mypetclinic.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+@MappedSuperclass
+public class BaseEntity implements Serializable  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isNew(){
+        return this.id == null;
+    }
+}
