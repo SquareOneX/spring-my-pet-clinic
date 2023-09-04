eb950e955f1cba284dd7ed2c15eff8fad9f281b7
lukas_laptop <lukasreiche@outlook.de>
2023-09-04 13:09:24 +0200

    Refactored Services to use a common Base Interface

--- my-pet-clinic-data/src/main/java/squareonex/mypetclinic/services/VetService.java
+++ my-pet-clinic-data/src/main/java/squareonex/mypetclinic/services/VetService.java
@@ -2,13 +2,7 @@ package squareonex.mypetclinic.services;
 
 import squareonex.mypetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet,Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
 
