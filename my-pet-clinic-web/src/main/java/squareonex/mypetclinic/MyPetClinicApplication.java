0e1afa164be57319d67849b49b9b213a5fee757b
lukas_laptop <lukasreiche@outlook.de>
2023-09-08 14:46:27 +0200

    Implemented list of vets with thymeleaf
    Closes #17

--- my-pet-clinic-web/src/main/java/squareonex/mypetclinic/MyPetClinicApplication.java
+++ my-pet-clinic-web/src/main/java/squareonex/mypetclinic/MyPetClinicApplication.java
@@ -2,16 +2,12 @@ package squareonex.mypetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import squareonex.mypetclinic.services.OwnerService;
 
 @SpringBootApplication
 public class MyPetClinicApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(MyPetClinicApplication.class, args);
-        OwnerService bean = context.getBean(OwnerService.class);
-        System.out.println(bean.findAll());
+        SpringApplication.run(MyPetClinicApplication.class, args);
     }
 
 }
