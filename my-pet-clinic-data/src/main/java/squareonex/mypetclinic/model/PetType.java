988bb8465261e5ec65882a38e579e9bf5cad641f
lukas_laptop <lukasreiche@outlook.de>
2023-09-17 13:31:20 +0200

    Added bootstrap data for pets
    Added lombok constructors to PetType pojo

--- my-pet-clinic-data/src/main/java/squareonex/mypetclinic/model/PetType.java
+++ my-pet-clinic-data/src/main/java/squareonex/mypetclinic/model/PetType.java
@@ -1,10 +1,14 @@
 package squareonex.mypetclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PetType {
     private String name;
 }
--- my-pet-clinic-web/src/main/java/squareonex/mypetclinic/bootstrap/DataLoader.java
+++ my-pet-clinic-web/src/main/java/squareonex/mypetclinic/bootstrap/DataLoader.java
@@ -3,19 +3,27 @@ package squareonex.mypetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import squareonex.mypetclinic.model.Owner;
+import squareonex.mypetclinic.model.Pet;
+import squareonex.mypetclinic.model.PetType;
 import squareonex.mypetclinic.model.Vet;
 import squareonex.mypetclinic.services.OwnerService;
+import squareonex.mypetclinic.services.PetService;
 import squareonex.mypetclinic.services.VetService;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetService petService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetService petService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petService = petService;
     }
 
     @Override
@@ -56,4 +64,22 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
+
+        Pet pet1 = new Pet();
+        pet1.setOwner(owner1);
+
+        PetType petType = new PetType("cat");
+
+        pet1.setPetType(petType);
+        pet1.setBirthDate(LocalDate.of(2022, Month.JANUARY, 20));
+        petService.save(pet1);
+
+        Pet pet2 = new Pet();
+        pet2.setOwner(owner2);
+
+        pet2.setPetType(petType);
+        pet2.setBirthDate(LocalDate.of(2022, Month.OCTOBER, 15));
+        petService.save(pet2);
+
+        System.out.println("Loaded Pets....");
     }}
